7b07ff1e43ab52781797179d636d764df62ddcbb
asyasmi <asya.smi@gmail.com>
2016-07-18 14:10:27 +0300

    initial project

---
+++ Network Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 offset;
+	private bool isPlayerFound = false;
+
+	// Use this for initialization
+	void Start () {
+		//cameraPos = transform.position;
+	}
+	void Update(){
+		player = GameObject.Find ("Sphere(Clone)");
+		if (player != null && !isPlayerFound) {
+			isPlayerFound = true; 
+			offset = transform.position - player.transform.position;
+		} 
+		/*else {
+			transform.position = cameraPos;
+			isPlayerFound = false;
+		}*/
+	}
+	// Update is called once per frame
+	void LateUpdate () {
+		if (player != null) {
+			transform.position = player.transform.position + offset;
+		}
+	}
+}
