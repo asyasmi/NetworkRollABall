4988d9910a75d327f946dd3eb4757cfc6a1992ca
asyasmi <asya.smi@gmail.com>
2016-08-30 19:20:28 +0300

    added text fields, updated spawning

---
+++ Network Roll-a-ball/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+
+	[SyncVar]
+	public int currentHealth = maxHealth;
+
+	[Command]
+	public void CmdTakeDamage(int amount){
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+			Debug.Log("Dead!");
+		}
+	}
+}
--- Network Roll-a-ball/Assets/Scripts/PlayerMovement.cs
+++ Network Roll-a-ball/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerMovement : NetworkBehaviour {
 
-	private Rigidbody rb;
-	public float Speed;
-	private GameObject[] points; 
-	private NetworkInstanceId ownerNetID; 
+	public float speed;
 	public ParticleSystem explosion;
 	public GameObject pickup;
+	public Text countText;
+	public Text winText;
+	public GameObject player;
+
+	[SyncVar]
+	private int score;
+
+	private Rigidbody rb;
+	private GameObject[] points; 
+	private NetworkInstanceId ownerNetID; 
+	private GameObject spawnPoint;
 
 	//On start of work
 	void Start (){
-		rb = GetComponent<Rigidbody> ();
-		points = GameObject.FindGameObjectsWithTag ("SpawnPoint");
 
+		// spawnPoint = GameObject.FindGameObjectWithTag ("SpawnPointPlayer"); 
+		//Instantiate (player, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+		rb = player.GetComponent<Rigidbody> ();
+
+		winText = GameObject.Find ("WinText").GetComponent<Text>();
+		countText = GameObject.Find ("CountText").GetComponent<Text>();
+		 
+		winText.text = "Hello! Start collecting! "; 
+		score = 0;	
+
+		points = GameObject.FindGameObjectsWithTag ("SpawnPoint");
 		for (int i = 0; i < points.Length; i++) {
-			Instantiate (pickup, points [i].transform.position, points [i].transform.rotation);
+			Instantiate (pickup, points[i].transform.position, points[i].transform.rotation);
 		}
+			
 	}
 		
 	public override void OnStartLocalPlayer()
@@ -28,6 +48,7 @@ public class PlayerMovement : NetworkBehaviour {
 
 
 	// Use this for initialization
+
 	void FixedUpdate () {
 		if (!isLocalPlayer) {
 			return;
@@ -35,7 +56,7 @@ public class PlayerMovement : NetworkBehaviour {
 			float moveHorizontal = Input.GetAxis ("Horizontal");
 			float moveVertical = Input.GetAxis ("Vertical");
 			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-			rb.AddForce (movement * Speed);
+			rb.AddForce (movement * speed);
 		}
 	}
 
@@ -44,24 +65,35 @@ public class PlayerMovement : NetworkBehaviour {
 		
 	}
 
+	IEnumerator CleanWinText() {
+		yield return new WaitForSeconds(5);
+		winText.text = ("");
+	}
+
+
+
 	void OnTriggerEnter(Collider other){
+		
+		if (winText.text != "") {
+			StartCoroutine(CleanWinText());
+		}
 
 		if (isLocalPlayer){
 			
 			if (other.gameObject.CompareTag("PickUp")){
-					CmdAssignAuthority(other.gameObject.GetComponent<NetworkIdentity>());
+				CmdAssignAuthority(other.gameObject.GetComponent<NetworkIdentity>());
 
-					Color myColour = new Color(Random.value, Random.value, Random.value);
-					other.GetComponent<Renderer> ().material.SetColor ("_Color", myColour);
+				Color myColour = new Color(Random.value, Random.value, Random.value);
+				other.GetComponent<Renderer> ().material.SetColor ("_Color", myColour);
 
-					Debug.Log ("Authority assigned to PickUp");
-					Debug.Log (other.gameObject.GetComponent<NetworkIdentity>().ToString());
+				CmdSetCountText (gameObject.GetComponent<NetworkIdentity>());
 
-					var Explosion = Instantiate (explosion, other.transform.position, other.transform.rotation);
-					Destroy (other.gameObject, 2.0f);
-					Destroy (Explosion, 2.0f);
-					
-					SpawnPickUp (other.transform.position, other.transform.rotation);
+				var Explosion = Instantiate (explosion, other.transform.position, other.transform.rotation);
+				
+				CmdDestroyPickUp (other.gameObject);
+				Destroy (Explosion, 2.0f);
+				
+				SpawnPickUp (other.transform.position, other.transform.rotation);
 
 				} else {
 					return;
@@ -70,28 +102,44 @@ public class PlayerMovement : NetworkBehaviour {
 	}
 
 
+	[Command]
+	void CmdSetCountText(NetworkIdentity netId){
+		countText.text = "Score: " + score.ToString () + ", for " + netId.ToString();
+
+		if (score >= 15) {
+			winText.text = "Victory!";
+		}
+	}
+
+	[Command]
+	void CmdDestroyPickUp(GameObject go){
+		Destroy (go, 2.0f);
+	}
+
 	void SpawnPickUp (Vector3 position, Quaternion rotation){
 
 		int i =Random.Range (0, points.Length - 1);
-		/*
-		point.transform.position.x = point.transform.position.x + Random.Range (-5.0f, 5.0f);
-		point.transform.position.z = point.transform.position.x + Random.Range (-5.0f, 5.0f);
-
-		point.transform.rotation.x = point.transform.rotation.x + Random.Range (-2.0f, 2.0f);
-		point.transform.rotation.z = point.transform.rotation.z + Random.Range (-2.0f, 2.0f);*/
 
 		var PickUp = Instantiate (pickup, points[i].transform.position, points[i].transform.rotation);
 	}
 
 	[Command]
 	void CmdAssignAuthority (NetworkIdentity netid){
-		if (netid.RemoveClientAuthority(connectionToClient))
-			netid.RemoveClientAuthority (connectionToClient);
+		if (netid.hasAuthority)
+			CmdRemoveClientAuthority (netid);
+		else
+			return;
 		
 		netid.AssignClientAuthority (connectionToClient);
+
+		Debug.Log (netId.ToString());
 		Debug.Log ("assigned!");
 	}
 
+	[Command]
+	void CmdRemoveClientAuthority(NetworkIdentity netid){
+		netid.RemoveClientAuthority (connectionToClient);
+	}
 
 
 
